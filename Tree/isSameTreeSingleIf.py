f9929313e7d786da69fd8d2a4c8fc954d65b9931
Shrinkhla Sharma <shrinkhla@ipsec-172-16-86-210.vpn.gatech.edu>
2019-08-25 16:30:52 -0400

    using single if condition

---
+++ Tree/isSameTreeSingleIf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right, q.right)
+        
+        return p is q.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+            
+    SolutionObject = Solution()
