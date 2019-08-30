d365e56384f38892c0094d2901acb5bfd3d961aa
Shrinkhla Sharma <shrinkhla@Shrinkhlas-MacBook-Pro.local>
2019-08-30 13:11:58 -0400

    O(logn) time and O(H) space

---
+++ Tree/searchBST.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root.left and root.val > val:
+            return self.searchBST(root.left, val)
+        if root.val == val:
+            return root
+        if root.right and root.val < val:
+            return self.searchBST(root.right, val)
+            
+#o(H) space and O(logn) time
