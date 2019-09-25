bd14c0f01437d1e71ff6945239064940eb41c89e
Shrinkhla Sharma <shrinkhlaGatech@users.noreply.github.com>
2019-09-25 19:17:24 -0400

    O(1) space and O(S) time

--- Hash Table/numJewelsInStones.py
+++ Hash Table/numJewelsInStones.py
@@ -11,5 +11,9 @@ class Solution:
             if stone in Jewel_hash:
                 number_jewels += 1
         return number_jewels
-#O(J) space for storing J in hash 
-#O(J+S) time for storing J in hash and iterating over S
+"""O(J) space for storing J in hash . We can consider 
+this as constant space because there will be 26*2 caracters at most so it is constant space """
+
+"""O(J+S) time for storing J in hash and iterating over S , however storing 
+J elements in hash will be constant time as a maximum of 26*2 characters .
+So, time complexity will be just O(S) time"""
